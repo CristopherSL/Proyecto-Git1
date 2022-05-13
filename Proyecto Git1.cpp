fcd7d7911620d1e24e947215144715ea04075762
Carlos Manuel Evangelista Fernandez <N00202953@upn.pe>
2022-05-12 20:09:10 -0500

    Se agrego el integrante

--- Proyecto Git1.cpp
+++ Proyecto Git1.cpp
@@ -8,5 +8,6 @@ int main()
     cout << "Hola, presentate por favor\n";
     cout << "Autor: Cristopher Soto\n";
     cout << "Autor: Nayeli Malca Zarate\n";
+    cout << "Autor: Carlos ManueL Evangelista\n";
 }
 
